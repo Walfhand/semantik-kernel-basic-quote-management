150d652851dbf737572392a8291452d42d25fa01
Sébastien <vandaele.seba@gmail.com>
2025-04-24 20:04:52 +0200

    Simplifies quote creation and management
    Removes the direct association between quotes and contacts, as well as organization customers.
    This change streamlines the quote creation and management process by decoupling it from specific contact and organization customer entities.

--- IaPluginPoc/Models/Contact.cs
+++
@@ -1,9 +0,0 @@
-namespace IaPluginPoc.Models;
-
-public record Contact
-{
-    public Guid Id { get; private set; } = Guid.NewGuid();
-
-    public string Firstname { get; set; } = null!;
-    public string Lastname { get; set; } = null!;
-}
--- IaPluginPoc/Models/Quote.cs
+++ IaPluginPoc/Models/Quote.cs
@@ -10,10 +10,8 @@ public class Quote
 
     public Guid Id { get; private set; } = Guid.NewGuid();
     public string ProblemDescription { get; private set; }
-    public Contact? Contact { get; private set; }
     public List<QuoteItem> QuoteItems { get; private set; } = [];
     public PersonCustomer? PersonCustomer { get; private set; }
-    public OrganizationCustomer? OrganizationCustomer { get; private set; }
     public int TotalPrice { get; private set; }
 
     public static Quote CreateMinimalValidQuote()
@@ -33,21 +31,6 @@ public class Quote
                                             Le sol devra être recarrelé avec des carreaux imitation bois, de dimension 20x120 cm, avec un budget de 30 €/m² maximum. 
                                             Enfin, il demande l’installation d’un meuble vasque suspendu avec miroir et éclairage intégré.
                                          """);
-    }
-
-    public void LinkContact(Contact contact)
-    {
-        Contact = contact;
-    }
-
-    public void LinkOrganizationCustomer(OrganizationCustomer organizationCustomer)
-    {
-        OrganizationCustomer = organizationCustomer;
-    }
-
-    public void LinkPersonCustomer(PersonCustomer personCustomer)
-    {
-        PersonCustomer = personCustomer;
     }
 
     public void AddQuoteItem(QuoteItem quoteItem)
